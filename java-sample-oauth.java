4b3eb849b0b412b1c9ac492ec972d2e52d5bf30b
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2019-06-16 15:31:28 +0430

    Update java-sample-oauth.java

--- java-sample-oauth.java
+++ java-sample-oauth.java
@@ -1,13 +1,20 @@
-SecureRandom sr = new SecureRandom();
-byte[] code = new byte[32];
-sr.nextBytes(code);
-String verifier = Base64.encodeToString(code, Base64.URL_SAFE | Base64.NO_WRAP | Base64.NO_PADDING);
 
-byte[] bytes = verifier.getBytes("US-ASCII");
-MessageDigest md = MessageDigest.getInstance("SHA-256");
-md.update(bytes, 0, bytes.length);
-byte[] digest = md.digest();
-//Use Apache "Commons Codec" dependency. Import the Base64 class
-//import org.apache.commons.codec.binary.Base64;
-String challenge = Base64.encodeBase64URLSafeString(digest);
-
+        SecureRandom sr = new SecureRandom();
+        byte[] code = new byte[32];
+        sr.nextBytes(code);
+        String verifier = android.util.Base64.encodeToString(code, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
+        byte[] bytes = new byte[0];
+        try {
+            bytes = verifier.getBytes("US-ASCII");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        md.update(bytes, 0, bytes.length);
+        byte[] digest = md.digest();
+        String challenge = android.util.Base64.encodeToString(digest, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
