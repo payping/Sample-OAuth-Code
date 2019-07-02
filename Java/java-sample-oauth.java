412fa69165c1fa7ad84b29aa5ba721fb96ec8db7
Ashkan Rahmani <ashkan.rmk@hotmail.com>
2019-07-02 10:04:15 +0430

    Auto stash before merge of "master" and "origin/master"

--- Java/java-sample-oauth.java
+++ Java/java-sample-oauth.java
@@ -1,20 +1,20 @@
 
-        SecureRandom sr = new SecureRandom();
-        byte[] code = new byte[32];
-        sr.nextBytes(code);
-        String verifier = android.util.Base64.encodeToString(code, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
-        byte[] bytes = new byte[0];
-        try {
-            bytes = verifier.getBytes("US-ASCII");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        md.update(bytes, 0, bytes.length);
-        byte[] digest = md.digest();
-        String challenge = android.util.Base64.encodeToString(digest, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
+SecureRandom sr = new SecureRandom();
+byte[] code = new byte[32];
+sr.nextBytes(code);
+String verifier = android.util.Base64.encodeToString(code, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
+byte[] bytes = new byte[0];
+try {
+    bytes = verifier.getBytes("US-ASCII");
+} catch (UnsupportedEncodingException e) {
+    e.printStackTrace();
+}
+MessageDigest md = null;
+try {
+    md = MessageDigest.getInstance("SHA-256");
+} catch (NoSuchAlgorithmException e) {
+    e.printStackTrace();
+}
+md.update(bytes, 0, bytes.length);
+byte[] digest = md.digest();
+String challenge = android.util.Base64.encodeToString(digest, android.util.Base64.URL_SAFE | android.util.Base64.NO_WRAP | android.util.Base64.NO_PADDING);
